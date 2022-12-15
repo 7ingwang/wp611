c9c1dfeb56c1e0df12a9e7238beaf8c0311aec38
Terry <terry@TerrysMacbookM1.lan>
2022-12-15 21:23:56 +0800

    footer add email

--- wp-content/themes/czqg_v2.0/footer.php
+++ wp-content/themes/czqg_v2.0/footer.php
@@ -7,8 +7,8 @@
     <div class="row">
       <div class="col-md-9">
         <?php get_template_part('inc/header/site-info');?>
-        <p class="contact"><span class="adress">地址：河北省沧州市河间市瀛州路街道团结大街16号</span><span class="tel">电话：0317-5580388</span></p>
-        <p class="copyright"><span>Copyright © 2022 沧州曲港高速公路建设有限责任公司 版权所有</span><span><a href="https://beian.miit.gov.cn/" target="_blank">冀ICP备8888888号</a></span></p>
+        <p class="contact"><span class="adress">地址：河北省沧州市河间市瀛州路街道团结大街16号</span><span class="tel">电话：0317-5580388</span><span class="email">邮箱：<a href="mailto:czqggs@163.com" target="_top">czqggs@163.com</a></span></p>
+        <p class="copyright"><span>Copyright © 2022 沧州曲港高速公路建设有限责任公司 版权所有</span><span><a href="https://beian.miit.gov.cn/" target="_blank">冀ICP备2022029595号-1</a></span></p>
       </div>
       <div class="col-md-3 d-flex justify-content-center">
         <div class="weixin text-center">
